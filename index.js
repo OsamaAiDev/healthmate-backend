bd76bbdcb44d2020d9aac173c9b1136997a8aa77
OsamaAiDev <osamafreelancer22@gmail.com>
2025-10-31 19:13:09 +0500

    cors 1

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
-const connectDB = require('./config/db');
-require('dotenv').config();
+const connectDB = require("./config/db");
+require("dotenv").config();
 
 const app = express();
 
 const corsOptions = {
-  origin: ['https://healthmate-frontend-gules.vercel.app', 'http://localhost:5173'],
-  optionsSuccessStatus: 200
-}
+  origin: [
+    "https://healthmate-frontend-gules.vercel.app",
+    "http://localhost:5173",
+  ],
+  optionsSuccessStatus: 200,
+};
 
 app.use(cors(corsOptions));
 
@@ -20,12 +23,12 @@ connectDB();
 
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.send('API Running'));
+app.get("/", (req, res) => res.send("API Running"));
 
 // Define Routes
-app.use('/api/auth', require('./routes/auth'));
-app.use('/api/reports', require('./routes/reports'));
-app.use('/api/vitals', require('./routes/vitals'));
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/reports", require("./routes/reports"));
+app.use("/api/vitals", require("./routes/vitals"));
 
 const PORT = process.env.PORT || 5000;
 
